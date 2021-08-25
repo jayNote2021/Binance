215a80d6916b1645e73b69de209534e7c283c4b4
jay <jaynote2021@gmail.com>
2021-08-26 03:23:37 +0900

    feat:   Project set up
    # app.js 생성
    - 서버를 동작하기 위한 기본 셋업 완료
    # config.js 생성
    - .env를 읽어 환경변수를 적용하고 사용하는 모듈

--- src/app.js
+++ src/app.js
@@ -1,1 +1,42 @@
+import express from 'express';
+// 공통: 헤더, 로그
+import cors from 'cors';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
+// 비동기 에러 처리 모듈
+import 'express-async-errors';
+// 설정
+import { config } from './config.js';
 
+// 라우터
+// config
+
+const app = express();
+
+// 미들웨어 설정
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan('dev'));
+app.use(cors());
+app.use(helmet());
+
+app.get('/', (req, res, next) => {
+  res.sendStatus(200);
+});
+
+// 라우트
+
+// 에러 미들웨어
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({ msg: 'server error' });
+});
+
+// 404 처리
+app.use((req, res, next) => {
+  console.log('[404 Not found] : ' + req.url);
+  res.sendStatus(404);
+});
+
+app.listen(config.host.port);
--- src/config.js
+++ src/config.js
@@ -1,1 +1,32 @@
+import dotenv from 'dotenv';
+import path from 'path';
 
+// 환경변수에 따른 .env* 파일 읽어오기
+dotenv.config({
+  path: path.resolve(
+    process.cwd(), // 프로젝트 루트 경로
+    process.env.NODE_ENV == 'production' //
+      ? '.env' // 서비스 환경인 경우
+      : '.env.dev' // 개발 환경인 경우
+  ),
+});
+
+function required(key, defaultValue = undefined) {
+  const value = process.env[key] || defaultValue;
+  // null 또는 undefined
+  if (value == null) {
+    throw new Error(`env key ${key} is undefined`);
+  }
+  return value;
+}
+
+// 환경변수를 읽어서 객체로 변환
+export const config = {
+  host: {
+    port: required('HOST_PORT', 8080),
+  },
+  binance: {
+    api_key: required('BINANCE_API_KEY'),
+    api_secret_key: required('BINANCE_API_SECRET_KEY'),
+  },
+};
